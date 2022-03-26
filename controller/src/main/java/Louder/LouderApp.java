2e04771dc088c18067f8f23e736d8ba3d6248a14
yuliamed <yuliamed@list.ru>
2022-03-26 19:46:34 +0300

    add classes

--- controller/src/main/java/Louder/LouderApp.java
+++ controller/src/main/java/Louder/LouderApp.java
@@ -1,18 +1,21 @@
 package Louder;
-
 import controller.UserController;
 import entity.User;
 import inteface.UserRepository;
 import mapper.UserMapper;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import service.UserServiceInt;
+import service.UserService;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackageClasses = {
+        LouderApp.class,
+        UserController.class,
+        UserMapper.class,
+        UserRepository.class,
+        UserService.class})
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 @EntityScan(basePackageClasses = User.class)
 public class LouderApp {
