9428a0524341c24085e7301bfa52f81175d6490a
yuliamed <yuliamed@list.ru>
2022-04-07 00:52:34 +0300

    add tracking abstract entity

---
+++ entity/src/main/java/by/iba/entity/TrackingAbstractEntity.java
@@ -0,0 +1,42 @@
+package by.iba.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@MappedSuperclass
+@NoArgsConstructor
+public abstract class TrackingAbstractEntity extends AbstractEntity {
+    @Column(name = "creation_date", nullable = false)
+    private LocalDateTime creationDate;
+    @Column(name = "last_update_date", nullable = false)
+    private LocalDateTime lastUpdateDate;
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+
+    @PrePersist
+    private void abstractEntityPreInit() {
+        this.version = 1;
+        this.creationDate = LocalDateTime.now();
+        this.lastUpdateDate = LocalDateTime.now();
+
+    }
+
+    @PreUpdate
+    private void abstractEntityPreUpdate() {
+        if (Objects.nonNull(this.version)){
+            this.version++;
+        }
+        this.lastUpdateDate = LocalDateTime.now();
+    }
+
+
+}
