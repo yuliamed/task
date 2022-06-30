d249eeba14258c4c7398cafa08b57bf0d073f7ab
yuliamed <yuliamed@list.ru>
2022-06-30 15:02:36 +0300

    add exception for wrong name of email box

--- service/src/main/java/by/iba/service/impl/MailServiceImpl.java
+++ service/src/main/java/by/iba/service/impl/MailServiceImpl.java
@@ -1,10 +1,14 @@
 package by.iba.service.impl;
 
+import by.iba.exception.ServiceException;
 import by.iba.service.MailService;
 import lombok.AllArgsConstructor;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+
+import javax.mail.AuthenticationFailedException;
 
 @Service
 @AllArgsConstructor
@@ -16,14 +20,17 @@ public class MailServiceImpl implements MailService {
     @Override
     public void sendEmail(String emailTo, String subject, String message) {
 
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        try {
             mailMessage.setFrom(sender);
             mailMessage.setTo(emailTo);
             mailMessage.setSubject(subject);
             mailMessage.setText("Hello, this message can help you to confirm your account " + message);
-
             this.javaMailSender.send(mailMessage);
+        } catch (MailException ex) {
+            throw new ServiceException("The mail box with name = " + emailTo + " does not exist! Check your email");
+        }
+
 
     }
 
